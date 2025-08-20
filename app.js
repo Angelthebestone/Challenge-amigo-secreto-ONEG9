f679acfb397b01a55932023ec1f86637b3218051
Angel Peña <penaagamezangeldavid@gmail.com>
2025-08-20 12:14:07 -0500

    Corrección de las funciones

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ let listaDeAmigos = [];
 function limpiarCampo() {
   document.getElementById("amigo").value = "";
 }
-function agregarAmigos() {
-    let nombre = document.getElementById('Amigo');
-    let verificarNombre = nombre.length
-     if (nombre === 0) {
-        alert("Debes rellenar con un nombre valido");
-
-     } else {
-      listaDeAmigos.push(nombre);
-      limpiarCampo();
-     }
+function agregarAmigo() {
+  let nombreInput = document.getElementById('Amigo');
+  let nombre = nombreInput.value.trim();
+  console.log(nombre);
+  let verificarNombre = nombre.length;
+  console.log(verificarNombre);
+  if (verificarNombre === 0) {
+    alert("Debes rellenar con un nombre valido");
+  } else {
+    listaDeAmigos.push(nombre);
+    limpiarCampo();
+  }
 }
