9d9ea84ec2dd3c44e42620b3804504f6be567e66
Angel Peña <penaagamezangeldavid@gmail.com>
2025-08-20 11:58:16 -0500

    Creación de funciones de limpiar y agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [];
+
+function limpiarCampo() {
+  document.getElementById("amigo").value = "";
+}
+function agregarAmigos() {
+    let nombre = document.getElementById('Amigo');
+    let verificarNombre = nombre.length
+     if (nombre === 0) {
+        alert("Debes rellenar con un nombre valido");
+
+     } else {
+      listaDeAmigos.push(nombre);
+      limpiarCampo();
+     }
+}
