23d8ee8b3f7c50317ec4a23382192ceffa89de17
Angel Peña <penaagamezangeldavid@gmail.com>
2025-08-20 12:28:16 -0500

    Comentarios en el codigo de JS para entender el funcionamiento del codigo

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ let listaDeAmigos = [];
 function limpiarCampo() {
   document.getElementById("amigo").value = "";
 }
-function agregarAmigo() {
-  let nombreInput = document.getElementById('Amigo');
-  let nombre = nombreInput.value.trim();
+function agregarAmigo() { 
+ 
+  let nombreInput = document.getElementById('amigo'); //extrae el nombre de la caja de texto
+
+  let nombre = nombreInput.value; //convierte la entrada HTML en un valor
   console.log(nombre);
-  let verificarNombre = nombre.length;
+  let verificarNombre = nombre.length; //verifica si el nombre tiene contenido
   console.log(verificarNombre);
-  if (verificarNombre === 0) {
+  if (verificarNombre === 0) { //ciclo if para verificar la validez del nombre
     alert("Debes rellenar con un nombre valido");
-  } else {
-    listaDeAmigos.push(nombre);
-    limpiarCampo();
+  } else { //si el nombre es valido
+    listaDeAmigos.push(nombre); //agrega el nombre a la lista de amigos
+    console.log(listaDeAmigos); //muestra la lista actualizada de amigos
+    limpiarCampo(); //limpia el campo de entrada
+    return; //finaliza la función
   }
 }
